ab39aaac003a3a38249baaf9184a2bfdffc9b2f0
kunjaimonai <yadukrishnanvs.official@gmail.com>
2025-05-12 21:27:41 +0530

    .

--- pwa/service-worker.js
+++
@@ -1,5 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/pwa/service-worker.js')
-    .then(() => console.log('Service Worker Registered'))
-    .catch(error => console.error('Service Worker registration failed:', error));
-}
