ee97945194714c4e2e539d0526a4cc51d6c0a250
kunjaimonai <yadukrishnanvs.official@gmail.com>
2025-05-13 14:36:18 +0530

    .

--- pwa/flutter_bootstrap.js
+++ pwa/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2729332407"
+    serviceWorkerVersion: "3597116292"
   }
 });
